3d3fb8f498c6952ef9d700a7d48e781e8edc3a75
LitYora <matveybess@gmail.com>
2020-12-02 12:17:18 +0300

    Initial Commit

---
+++ PictureCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PictureCollectionView
+//
+//  Created by Matvei  on 02.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
